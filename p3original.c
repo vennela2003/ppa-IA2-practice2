ab6ee38a13998afcc477efa81e17679c547b636f
vennela_sowjanya <94883183+vennela2003@users.noreply.github.com>
2022-03-22 03:09:24 +0000

    executed

--- p3original.c
+++ p3original.c
@@ -1,50 +1,48 @@
-#include <stdio.h>
+#include<stdio.h>
 int input_number()
 {
-  int n;
-  printf("enter the number:\n");
-  scanf("%d",&n);
-  return n;
+int n;
+printf("Enter a number: \n");
+scanf("%d",&n);
+return n;
 }
 int is_composite(int n)
 {
-  int i;
-  if(n==0 || n==1)
+int i,flag=0;
+
+if(n==0||n==1)//The number 0 & 1 is neither prime nor composite number
+return 2;
+else
   {
-    return 2;
+for (i = 2; i <= n / 2; ++i)
+  {
+    // if n is divisible by i, then n is not prime
+    // change flag to 1 for non-prime number
+if (n % i == 0) 
+    {
+flag = 1;
+break;
+    }
   }
-  else
-  {
-    for(i=2;i<=n/2;i++)
-      {
-        if(n%i==0)
-        {
-          return 1;
-        }
-      }
-    return 0;
+return flag;  
   }
 }
-void output(int n, int composite)
+
+void output(int n,int composite)
 {
-  if(composite==0)
-    printf("the number %d is prime number",n);
-  else if(composite==1)
-    printf("the  number %d is composite number",n);
-  else
-    printf("the number 0&1 is neither prime nor composite number");
-  }
+
+if(composite==0)
+printf("The number %d is prime number",n);
+else if(composite==1)
+printf("The number %d is composite number",n);
+else
+printf("The number 0 & 1 is neither prime nor composite number");
+}
 int main()
 {
-  int n,composite;
-  n=input_number();
-  composite=is_composite(n);
-  output(n,composite);
-  return 0;
+int n,composite;
+n=input_number();
+composite=is_composite(n);
+output(n,composite);
+return 0;
 }
-
-  
-
-
-
-
